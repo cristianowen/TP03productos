030a48ce9f0607e99c7f8ace32a65f3f80509f6e
cristianowen <cristian.owen.chen@gmail.com>
2023-04-26 09:21:37 -0300

    clase26/4

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 function productos() {
-    let datos = document.getElementById("search-input").value;
-    console.log("Hago consulta - fetch");
-    let card;
-    let search = document.getElementById("search-body");
-    fetch(`https://dummyjson.com/products/search?q=${datos}`)
-      .then(res => res.json())
-      .then(res => {
-        console.log("obtuve respuesta")
-        search.innerHTML = card;
-        res.products.forEach(actual => {
-          card = `
-                <div class="card" style="width: 18rem;">
+  let datos = document.getElementById("search-input").value;
+  console.log("Hago consulta - fetch");
+  let card;
+  let search = document.getElementById("search-body");
+  fetch(`https://dummyjson.com/products/search?q=${datos}`)
+    .then(res => res.json())
+    .then(res => {
+      console.log("obtuve respuesta")
+      res.products.forEach(actual => {
+        card = `
+                <div class="card text-white bg-dark mb-3" style="width: 18rem;">
                     <img src="${actual.thumbnail}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${actual.title}</h5>
@@ -21,28 +20,17 @@ function productos() {
                         </button>
                     </div>
                 </div>
-                <!-------------------------- Modal -------------------------->
-                <div class="modal fade" id="exampleModal${actual.id}  " tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                  <div class="modal-dialog">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                      </div>
-                      <div class="modal-body">
-                        ${actual.description}
-                      </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
                 `
+        if (actual == res.products[0]) {
+          search.innerHTML = card;
+        }
+        else {
           search.innerHTML += card;
-        });
-      })
-      .catch(err => console.error("error", err));
-    console.log("Fin consulta - fetch");
-    return alert("los datos cargaron correctamente");
+        }
+
+      });
+    })
+    .catch(err => console.error("error", err));
+  console.log("Fin consulta - fetch");
+  return alert("los datos cargaron correctamente");
 }
